bbc5e5d3093998677e2d1eaf26a5e5708e73da3c
chekhov4 <n.chekhov@gmail.com>
2016-10-20 20:31:45 +0300

    Add files via upload

---
+++ solar_system_dictionary.py
@@ -0,0 +1,16 @@
+def solar_system():
+
+    dictionary = {
+        "Mercury": 57000000,
+        "Venus": 108000000,
+        "Earth": 150000000,
+        "Mars": 227000000,
+        "Jupiter": 778000000,
+        "Saturn": 1427000000,
+        "Uranus": 2871000000,
+        "Neptune": 4497000000
+        }
+    return dictionary
+
+if __name__ == '__main__':
+    print solar_system()
