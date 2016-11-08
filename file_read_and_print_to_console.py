0166215f1fba34d8edce813d4b8b157809c0d217
Nikolay S <n.chekhov@gmail.com>
2016-11-08 10:38:29 +0300

    Update file_read_and_print_to_console.py

--- file_read_and_print_to_console.py
+++ file_read_and_print_to_console.py
@@ -3,16 +3,10 @@ def print_file_content(file_path):
         print_file = open(file_path, 'r')
     except IOError:
         print('File cannot be opened')
-
-
     else:
         print(print_file.read())
         print_file.close()
 
-
-
 if __name__ == '__main__':
-
     file_path = 'c:\\temp\\test.txt'
-
     print_file_content(file_path)
