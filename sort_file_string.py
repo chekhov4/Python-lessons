7d9c0390d63f4bef6c88c899ecca17f9346962ab
Nikolay S <n.chekhov@gmail.com>
2016-11-08 10:43:37 +0300

    Update sort_file_string.py

--- sort_file_string.py
+++ sort_file_string.py
@@ -1,5 +1,4 @@
 def sort_file_strings(file_path):
-
     try:
         s_file = open(file_path, 'r+')
     except IOError:
@@ -11,9 +10,6 @@ def sort_file_strings(file_path):
     s_file.writelines(sorted(file_strings, key = len))
     s_file.close()
 
-
-
 if __name__ == '__main__':
-
     file_path = 'c:\\temp\\test.txt'
     sort_file_strings(file_path)
