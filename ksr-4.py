12b96e7c338ab6b2fbf059e3579240dd036b9814
Nikolay S <n.chekhov@gmail.com>
2016-11-08 20:00:46 +0300

    Add files via upload

---
+++ ksr-4.py
@@ -0,0 +1,20 @@
+def delete_extra_spaces(source_string):
+    words = source_string.split(' ')
+    targer_string = ''
+    double_space = False
+    for word in words:
+        if word == '' and double_space == True:
+            continue
+        if word == '':
+            targer_string = targer_string + ' '
+            double_space = True
+            continue
+            
+        targer_string = targer_string + word
+        double_space = False
+    print(targer_string)
+
+
+if __name__ == '__main__':
+    source_string = ' 11   2  gfdfgdf '
+    delete_extra_spaces(source_string)
